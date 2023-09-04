6b3235eadfe3d6b9a05f0fcc26bbd7171178872b
Arthur <arthur.snascimento01@gmail.com>
2023-09-04 13:20:33 -0300

    first commit

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,5 +1,5 @@
 import { ITarefa } from '../../../types/tarefa';
-import style from '../Lista.module.scss';
+import style from './Item.module.scss';
 
 interface Props extends ITarefa {
     selecionaTarefa: (tarefaSelecionada: ITarefa) => void
@@ -14,11 +14,10 @@ export default function Item(
         id,
         selecionaTarefa
     }: Props) {
-    console.log('item atual: ', {tarefa, tempo, selecionado, completado, id})
     return (
         <li 
-        className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`} 
-        onClick={() => selecionaTarefa({
+        className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : ''}`} 
+        onClick={() => !completado && selecionaTarefa({
             tarefa,
             tempo,
             selecionado,
@@ -27,6 +26,7 @@ export default function Item(
         })}>
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
+            {completado && <span className={style.concluido} aria-label="tarefa completada"></span>}
         </li>
     ) 
 }
